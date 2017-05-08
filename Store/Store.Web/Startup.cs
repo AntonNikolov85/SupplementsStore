1e7023a6677caa69080dd6ef8f479c11baa45d19
Anton Nikolov <antonio85pd@gmail.com>
2017-05-08 14:28:25 +0300

    add admin and roles
    create method to seed admini user and roles in database

--- Store/Store.Web/Startup.cs
+++ Store/Store.Web/Startup.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using Store.Data;
+using Store.Models.EntityModels;
 
 [assembly: OwinStartupAttribute(typeof(Store.Web.Startup))]
 namespace Store.Web
@@ -9,6 +13,61 @@ namespace Store.Web
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            CreateRolesandUsers();
+        }
+
+        private void CreateRolesandUsers()
+        {
+            StoreDbContext context = new StoreDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+
+            //creating first Admin Role and creating a default Admin User    
+            if (!roleManager.RoleExists("Admin"))
+            {
+
+                //create Admin role   
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                //create a Admin super user who will maintain the website                  
+                var user = new ApplicationUser();
+                user.UserName = "toniN";
+                user.Name = "Antonio";
+                user.Email = "antonio@abv.bg";
+
+                string userPassword = "An123$";
+
+                var chkUser = UserManager.Create(user, userPassword);
+
+                //add default User to Role Admin   
+                if (chkUser.Succeeded)
+                {
+                    var result1 = UserManager.AddToRole(user.Id, "Admin");
+
+                }
+            }
+
+            //creating Creating Moderator role    
+            if (!roleManager.RoleExists("Moderator"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Moderator";
+                roleManager.Create(role);
+
+            }
+
+            //creating Creating User role    
+            if (!roleManager.RoleExists("User"))
+            {
+                var role = new IdentityRole();
+                role.Name = "User";
+                roleManager.Create(role);
+
+            }
         }
     }
 }
