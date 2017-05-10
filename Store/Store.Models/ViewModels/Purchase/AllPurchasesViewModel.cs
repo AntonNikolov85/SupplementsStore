85c020555ee53752f3d1590ee3fa8acfd9aa0ded
Anton Nikolov <antonio85pd@gmail.com>
2017-05-10 12:30:26 +0300

    add some funtionality

---
+++ Store/Store.Models/ViewModels/Purchase/AllPurchasesViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Store.Models.ViewModels.Purchase
+{
+    using EntityModels;
+    using Infrastructure.Mapping;
+    using System.ComponentModel.DataAnnotations;
+
+    public class AllPurchasesViewModel : IMapFrom<Purchase>
+    {
+        public int Id { get; set; }
+
+        [Range(1, 999.999, ErrorMessage = "You are not allowed to buy products with such high Total Price")]
+        public decimal TotalPrice { get; set; }
+    }
+}
---
+++ Store/Store.Web/Controllers/PurchaseController.cs
@@ -0,0 +1,103 @@
+﻿namespace Store.Web.Controllers
+{
+    using Infrastructure.Mapping;
+    using Models.ViewModels.Purchase;
+    using Services.Contracts;
+    using System.Linq;
+    using System.Web.Mvc;
+
+    [RoutePrefix("purchase")]
+    public class PurchaseController : Controller
+    {
+        private IPurchaseService purchaseService;
+
+        public PurchaseController(IPurchaseService service)
+        {
+            this.purchaseService = service;
+        }
+
+
+        // GET: Purchase
+        [HttpGet]
+        [Route("all")]
+        [AllowAnonymous]
+        public ActionResult All()
+        {
+            var allSuppliers = this.purchaseService.GetAll().To<AllPurchasesViewModel>().ToList();
+
+            return View(allSuppliers);
+        }
+
+        // GET: Purchase/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Purchase/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Purchase/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Purchase/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Purchase/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Purchase/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Purchase/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
